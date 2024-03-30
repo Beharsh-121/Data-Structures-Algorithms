21e478bad29fe5c846ae21e6d13adf5133e41194
Ashhru <31harsh.tiwari@gmail.com>
2024-03-30 11:56:44 +0530

    did leetcode 962 approach 1

---
+++ Sliding-Window/962-Subarray-with-k-different-integers.cpp
@@ -0,0 +1,20 @@
+//T.C- O(n), S.C- O(1)
+int slidingWindow(vector<int>& nums, int k){
+        unordered_map<int,int>mp;
+        int i = 0, j = 0, n = nums.size(), count = 0;
+
+        while(j < n){
+            mp[nums[j]]++;
+            while(mp.size() > k){
+                mp[nums[i]]--;
+                if(mp[nums[i]] == 0) mp.erase(nums[i]);
+                i++;
+            }
+            count+= (j-i+1);
+            j++;
+        }
+        return count;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return slidingWindow(nums, k) - slidingWindow(nums, k-1);
+    }
