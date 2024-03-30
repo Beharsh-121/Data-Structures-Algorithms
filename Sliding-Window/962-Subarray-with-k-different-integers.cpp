f03f18e9a881a0cc1bd4be046d226a0589bd5573
Ashhru <31harsh.tiwari@gmail.com>
2024-03-30 17:28:02 +0530

    updated second approach

--- Sliding-Window/962-Subarray-with-k-different-integers.cpp
+++ Sliding-Window/962-Subarray-with-k-different-integers.cpp
@@ -18,3 +18,26 @@ int slidingWindow(vector<int>& nums, int k){
     int subarraysWithKDistinct(vector<int>& nums, int k) {
         return slidingWindow(nums, k) - slidingWindow(nums, k-1);
     }
+//T.c- O(3n) ,S.C - O(n)
+int subarraysWithKDistinct(vector<int>& nums, int k) {
+        int n = nums.size(), i = 0, j = 0, i_bada = 0, result = 0;
+        unordered_map<int,int> mp;
+
+        while(j < n){
+          mp[nums[j]]++;
+          while(mp.size() > k){
+            mp[nums[i]]--;
+            if(mp[nums[i]] == 0) mp.erase(nums[i]);
+            i++;
+            i_bada = i;
+          }
+          while(mp[nums[i]] > 1){
+            mp[nums[i]]--;
+            if(mp[nums[i]] == 0) mp.erase(nums[i]);
+            i++;
+          }
+          if(mp.size() == k) result += (1 + i - i_bada);
+          j++;
+        }
+        return result;
+    }
