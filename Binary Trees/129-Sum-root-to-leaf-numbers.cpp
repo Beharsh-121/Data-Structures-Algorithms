a3b4647fb2e3ff84f761be02f85d10b816f7b7de
Ashhru <31harsh.tiwari@gmail.com>
2024-04-15 17:42:25 +0530

    added the binary tree folder

---
+++ Binary Trees/129-Sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,11 @@
+int solve(TreeNode* root, int curr){
+        if(!root) return 0;
+        curr = (curr*10) + root->val;
+        if(!root->left && !root->right) return curr;
+        int left = solve(root->left, curr);
+        int right = solve(root->right, curr);
+        return left + right;
+    }
+    int sumNumbers(TreeNode* root) {
+        return solve(root, 0);
+    }
